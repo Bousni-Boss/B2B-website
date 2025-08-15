9dcb86a33cf03f2ec99c815b1a4c449e56c7e56f
Bousni-Boss <switchmr.jeff911@gmail.com>
2025-08-16 00:22:22 +0100

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,11 +1,8 @@
 // app/api/login/route.ts
-- import { login } from '@/lib/auth';
 + import { login } from '../../../lib/auth';
 
 // app/api/logout/route.ts
-- import { logout } from '@/lib/auth';
 + import { logout } from '../../../lib/auth';
 
 // app/api/whoami/route.ts
-- import { getSessionFromCookie } from '@/lib/auth';
 + import { getSessionFromCookie } from '../../../lib/auth';
