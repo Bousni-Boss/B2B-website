42419f53fddd6b932c5ba18550267e876b9b0df5
Bousni-Boss <switchmr.jeff911@gmail.com>
2025-08-16 00:20:58 +0100

    Update ColorQtyMatrix.tsx

--- components/ColorQtyMatrix.tsx
+++ components/ColorQtyMatrix.tsx
@@ -1,11 +1,7 @@
 // components/ColorQtyMatrix.tsx
-- import type { VariantRow } from '@/lib/types';
-- import type { GemPolicy } from '@/lib/gem-policy';
 + import type { VariantRow } from '../lib/types';
 + import type { GemPolicy } from '../lib/gem-policy';
 
 // components/SpecFieldset.tsx
-- import { CATEGORY_FIELDS } from '@/lib/category-fields';
-- import type { SpecField } from '@/lib/types';
 + import { CATEGORY_FIELDS } from '../lib/category-fields';
 + import type { SpecField } from '../lib/types';
