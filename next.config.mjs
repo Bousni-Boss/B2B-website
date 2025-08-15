9209b9c7c4037cbe78c482a608e1d2b251a93d4d
Bousni-Boss <switchmr.jeff911@gmail.com>
2025-08-15 23:00:31 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,10 @@ import path from 'node:path';
 const nextConfig = {
   experimental: { serverActions: { allowedOrigins: ['*'] } },
   webpack: (config) => {
-    config.resolve.alias = config.resolve.alias || {};
-    config.resolve.alias['@'] = path.resolve(process.cwd());
+    config.resolve.alias ??= {};
+    config.resolve.alias['@'] = path.resolve(process.cwd()); // "@/..." -> project root
     return config;
   },
 };
 
 export default nextConfig;
-
