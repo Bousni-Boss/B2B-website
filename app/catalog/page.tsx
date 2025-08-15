845ef18750994d024bc95f965dc879b5ed2ea0d1
Bousni-Boss <switchmr.jeff911@gmail.com>
2025-08-16 00:21:32 +0100

    Update page.tsx

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,9 +1,4 @@
-- import NavBar from '@/components/NavBar';
-- import { getCategoryFromRef, CATEGORY_ORDER } from '@/lib/categories';
-- import { PRODUCTS } from '@/lib/products';
-- import { getGemPolicyForCategory } from '@/lib/gem-policy';
-- import ColorQtyMatrix from '@/components/ColorQtyMatrix';
-- import SpecFieldset from '@/components/SpecFieldset';
+
 + import NavBar from '../../components/NavBar';
 + import { getCategoryFromRef, CATEGORY_ORDER } from '../../lib/categories';
 + import { PRODUCTS } from '../../lib/products';
