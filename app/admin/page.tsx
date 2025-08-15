499512c1818d4fd763f65deea28f4b05114e80c5
Bousni-Boss <switchmr.jeff911@gmail.com>
2025-08-16 00:21:51 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,4 @@
 // app/admin/page.tsx
-- import NavBar from '@/components/NavBar';
-- import { getSessionFromCookie } from '@/lib/auth';
-- import { CLIENTS } from '@/lib/data';
-- import { PRODUCTS } from '@/lib/products';
 + import NavBar from '../../components/NavBar';
 + import { getSessionFromCookie } from '../../lib/auth';
 + import { CLIENTS } from '../../lib/data';
